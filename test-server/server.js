3bde2a669e0a70c32d2c28010d581a961e41070a
ejzimmer <ejzimmer@gmail.com>
2018-07-15 21:56:42 +1000

    Add readme and tidy up express server

--- test-server/server.js
+++ test-server/server.js
@@ -7,8 +7,6 @@ app.get('/properties', (req, res) => {
   res.send(properties);
 });
 
-app.use(express.static('public'));
-
 app.listen(7000, () => {
   console.log(`Example app listening on port 7000!`);
 });
